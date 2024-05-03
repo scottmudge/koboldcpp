0532b5e009643e2f0d8c323c59992ec522427a28
Scott Mudge <mail@scottmudge.com>
2024-05-03 08:56:07 -0400

    .

--- build.bat
+++ build.bat
@@ -1,1 +1,6 @@
-call .\venv\Scripts\activate.bat && cd "%~dp0" && make_pyinstaller_cuda.bat
+@echo off
+echo "Building KoboldCPP Custom You Beautiful Bitch! :)"
+copy .\custom.ico .\venv\Lib\site-packages\customtkinter\assets\icons\CustomTkinter_icon_Windows.ico /Y
+copy .\custom.ico .\venv\Lib\site-packages\PyInstaller\bootloader\images\icon-console.ico /Y
+copy .\custom.ico .\venv\Lib\site-packages\PyInstaller\bootloader\images\icon-windowed.ico /Y
+call .\venv\Scripts\activate.bat && cd "%~dp0" && make_pyinstaller_cuda12.bat
